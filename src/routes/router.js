23337b22544889e8224a273de669a1027c05740d
Aliaskei Saroka <lehasvv2009@gmail.com>
2024-03-19 22:03:55 +0300

    Change route history to hash cause of gh-pages

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 
 import AboutPage from "@/pages/AboutPage.vue";
 import MainPage from "@/pages/MainPage.vue";
@@ -30,7 +30,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes
 })
 
