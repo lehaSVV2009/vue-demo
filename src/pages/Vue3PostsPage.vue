94dfa9923b9d3b4ec1197e3c234e07d3fcc899e6
Aliaskei Saroka <lehasvv2009@gmail.com>
2024-03-19 21:23:01 +0300

    Add likes example to vue3 posts page

--- src/pages/Vue3PostsPage.vue
+++ src/pages/Vue3PostsPage.vue
@@ -1,11 +1,24 @@
 <template>
-  <h1>Vue 3 Posts</h1>
+  <div>Likes: {{ this.likes }}</div>
+  <v-btn @click="incrementLikes">Like</v-btn>
 </template>
 
 <script>
+import { onMounted, ref } from "vue";
 export default {
   setup() {
+    const likes = ref(0);
     
+    onMounted(() => {
+      console.log(likes);
+    })
+
+    return { likes };
   },
-}
+  methods: {
+    incrementLikes() {
+      this.likes++;
+    }
+  }
+};
 </script>
