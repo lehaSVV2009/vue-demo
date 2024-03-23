779c50efb3706328fe973f714136b42eb6461b6e
Aliaskei Saroka <lehasvv2009@gmail.com>
2024-03-23 21:19:28 +0300

    Add about to main page

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,6 +1,7 @@
 <template>
   <vue-viewport>
     <h1>Vue Posts Demo</h1>
+    <div>App to learn vue basics (vuex, vuetify, vue3)</div>
     <v-btn v-if="$store.state.auth.isLoggedIn" @click="logout">Logout</v-btn>
     <v-btn v-else @click="login">Login</v-btn>
   </vue-viewport>
@@ -10,10 +11,10 @@
 export default {
   methods: {
     login() {
-      this.$store.commit('auth/login')
+      this.$store.commit("auth/login");
     },
     logout() {
-      this.$store.commit('auth/logout')
+      this.$store.commit("auth/logout");
     },
   },
 };
