f548b1ca562113cad0e48c05ec7fe140afe15bcb
Aliaskei Saroka <lehasvv2009@gmail.com>
2024-03-23 16:47:32 +0300

    Make navbar responsive

--- src/pages/NavBar.vue
+++ src/pages/NavBar.vue
@@ -2,17 +2,37 @@
   <v-layout>
     <v-app-bar :elevation="2">
       <v-app-bar-title @click="$router.push('/')">My Posts</v-app-bar-title>
-      <template v-slot:append>
-        <v-btn @click="$router.push('/vuex-posts')">Vuex Posts</v-btn>
-        <v-btn @click="$router.push('/vue3-posts')">Vue3 Posts</v-btn>
-        <v-btn @click="$router.push('/about')">About</v-btn>
+      <template v-if="$vuetify.display.mdAndUp" v-slot:append>
+        <v-btn
+          v-for="menuButton in menuButtons"
+          :key="menuButton.url"
+          @click="$router.push(menuButton.url)"
+          >{{ menuButton.label }}</v-btn
+        >
+      </template>
+      <template v-else v-slot:append>
+        <v-menu>
+          <template v-slot:activator="{ props }">
+            <v-btn v-bind="props"> More </v-btn>
+          </template>
+          <v-list>
+            <v-list-item v-for="menuButton in menuButtons" :key="menuButton.url" @click="$router.push(menuButton.url)">
+              <v-list-item-title>{{ menuButton.label }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
       </template>
     </v-app-bar>
   </v-layout>
 </template>
 
-<script>
-export default {};
+<script setup>
+const menuButtons = [
+  { label: "Vuex Posts", url: "/vuex-posts" },
+  { label: "Vue3 Posts", url: "/vue3-posts" },
+  { label: "Vue3 Todos", url: "/vue3-todos" },
+  { label: "About", url: "/about" },
+];
 </script>
 
 <style scoped>
